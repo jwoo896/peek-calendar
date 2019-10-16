97546cc3e5197244845798d28fa66d33f62245e7
Joseph Woo <jwoo@Josephs-Air.home>
2019-10-16 16:27:25 -0700

    useParams is not a react hook, it is a function from react-router-dom

--- src/components/DayView.js
+++ src/components/DayView.js
@@ -1,14 +1,14 @@
-import React, { useParams } from 'react';
+import React  from 'react';
+import { useParams } from 'react-router-dom';
 import ActivityDetails from "./ActivityDetails";
 export default function DayView() {
 
     let { date } = useParams();
-
     const renderActivityDetails = () => {
         //will return JSX for activity details
     };
 
     return (
-      <div>{{ date }}</div>
+      <div>{date}</div>
     );
 }
