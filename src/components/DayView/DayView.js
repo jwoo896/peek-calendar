8569fcef215ccc7937401ad114ebcee4aeeddbdd
Joseph Woo <jwoo@Josephs-Air.home>
2019-10-18 15:53:09 -0700

    implemented material-ui button, DayView renders list of times slots per hr and populates section with activity name if there is an activity for that time slot. TODO button should show activity details to the right of the list

--- src/components/DayView/DayView.js
+++ src/components/DayView/DayView.js
@@ -1,55 +1,65 @@
 import React, { useState }  from 'react';
 import { useParams } from 'react-router-dom';
 import ActivityDetails from "../ActivityDetails/ActivityDetails";
-import { makeStyles } from '@material-ui/core/styles';
+import Button from "@material-ui/core/Button";
 import * as moment from 'moment';
 import * as day1data from "../../mock-data/day_1";
 import './DayView.css';
 
 export default function DayView() {
-    const [timeslots, setTimeSlots] = useState([]);
+    const [timeslots, setTimeSlots] = useState({});
+    const [selectedActivity, setSelectedAtivity] = useState('');
+    const [showActivityDetails, setShowActivityDetails] = useState(false);
+
+    let { date } = useParams();
     const dateStart = moment(day1data.timeslots[0].start);
     const dayBeforeStart = moment(day1data.timeslots[0].start).date(dateStart.date()-1);
-    let { date } = useParams();
     const dateObj = moment(date);
-    if(timeslots.length === 0 && dateObj <= dateStart && dateObj > dayBeforeStart) {
-        setTimeSlots(day1data.timeslots);
-    }
-    console.log(timeslots);
-    const renderActivityDetails = () => {
-        //will return JSX for activity details
-    };
 
-    const useStyles = makeStyles(theme => ({
-        root: {
-            width: '100%',
-            height: 400,
-            maxWidth: 360,
-            backgroundColor: theme.palette.background.paper,
+    const getActivitiesByStartHr = () => {
+        let tempObj = {};
+        for(let [key, val] of Object.entries(day1data.timeslots)){
+            tempObj = {...tempObj, [moment(val.start).hour()]: day1data.timeslots[key]};
         }
-    }));
+        setTimeSlots(tempObj);
+    }
+
+    const toggleShowActivityDetails = (e) => {
+        if(e.currentTarget.value !== '') {
+            setSelectedAtivity(e.currentTarget.value);
+            setShowActivityDetails(true);
+        }
+    }
 
     const RenderRows = () => {
         return (
             [...Array(24)].map((x, i) => {
                     return (
-                        <div className="timeslots-list-item">
+                        <div className="timeslots-list-item" key={i}>
                             <div style={{minWidth: '65px', textAlign: 'right', maxWidth: '65px'}}>
                                         <span style={{top: '-12px', left: '8px', position: 'relative'}}>
                                             {i === 0 ? ' \u00A0 ' : i < 12 ? `${i}:00 am` :
                                                 i === 12 ? `${i}:00 pm` : `${i - 12}:00 pm`}
                                         </span>
                             </div>
-                            <div style={{
+                            <Button
+                                value={timeslots.hasOwnProperty(i.toString()) ? timeslots[i].id : ''}
+                                onClick={toggleShowActivityDetails}
+                                style={{
                                 marginLeft: '1em', paddingTop: '1em', paddingLeft: '1em',
-                                paddingRight: '1em', borderBottom: `${i === 23 ? '' : '1px solid black'}`
-                            }}>
-                                some activity
-                            </div>
+                                paddingRight: '1em', width: '100%', borderRadius: '0px',
+                                borderBottom: `${i === 23 ? '' : '1px solid black'}`}}
+                            >
+                                {timeslots.hasOwnProperty(i.toString()) ? timeslots[i].activity_name : ''}
+                            </Button>
                         </div>
                     );
             })
         );
+    }
+
+    if(Object.values(timeslots).length === 0 && dateObj <= dateStart && dateObj > dayBeforeStart) {
+        getActivitiesByStartHr();
     }
 
     return (
@@ -61,7 +71,7 @@ export default function DayView() {
                 </div>
             </div>
             <div className="activity-details-container"> {/*this will be be hidden unless an activity is selected*/}
-                <ActivityDetails/>
+                {showActivityDetails === true ? <ActivityDetails activityId={selectedActivity}/> : ''}
             </div>
         </div>
     );
